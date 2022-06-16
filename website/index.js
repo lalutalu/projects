dcb5838316bbdee182783ebfd4ee5a362d3568a6
Lukas Alutalu <alutalu.lukas@outlook.com>
2022-06-16 17:14:18 -0400

    before sprite movement

--- website/index.js
+++ website/index.js
@@ -1,47 +1,70 @@
 // Make HTML element canvas appear
-const canvas = document.querySelector('canvas')
-const c = canvas.getContext('2d')
+const canvas = document.querySelector("canvas");
+const c = canvas.getContext("2d");
 
 // size of canvas
-canvas.width = 1024
-canvas.height = 576
+canvas.width = 1024;
+canvas.height = 576;
 
-c.fillStyle = 'white'
-c.fillRect(0, 0, canvas.width, canvas.height)
+c.fillStyle = "white";
+c.fillRect(0, 0, canvas.width, canvas.height);
 
 // background png
-const image = new Image()
-image.src = './img/Pellet Town.png'
+const image = new Image();
+image.src = "./img/Pellet Town.png";
 
 // 4 side by side playerDown Image
-const playerImage = new Image()
-playerImage.src = './img/playerDown.png'
+const playerImage = new Image();
+playerImage.src = "./img/playerDown.png";
 
-// Make background and player appear centered on screen 
-image.onload = () => {
-    c.drawImage(image, -750, -600)
-    c.drawImage(
+class Sprite {
+  constructor({ position, velocity }) {
+    this.position = position;
+  }
+
+    draw() {
         
-        // cropping
-        playerImage,
-        0,
-        0,
-        playerImage.width / 4,
-        playerImage.height,
-       
-        // actual width and height of image rendered on screen
-        canvas.width / 2 - playerImage.width / 4,
-        canvas.height / 2 - playerImage.height / 2,
-        playerImage.width / 4,
-        playerImage.height,
-
-    )
+    }
 }
 
-window.addEventListener('keydown', (e) => {
-    switch (e.key) {
-        case: 'w': 
+const background = new Sprite({ position: {
+    x: -742,
+    y: -600
+} })
 
-            break
-    }
-})
+function animate() {
+  window.requestAnimationFrame(animate);
+  // Make background and player appear centered on screen
+  c.drawImage(image, -742, -600);
+  c.drawImage(
+    // cropping
+    playerImage,
+    0,
+    0,
+    playerImage.width / 4,
+    playerImage.height,
+    // actual width and height of image rendered on screen
+    canvas.width / 2 - playerImage.width / 4 / 2,
+    canvas.height / 2 - playerImage.height / 2,
+    playerImage.width / 4,
+    playerImage.height
+  );
+}
+animate();
+
+window.addEventListener("keydown", (e) => {
+  switch (e.key) {
+    case "w":
+      console.log("pressed w key");
+      break;
+    case "a":
+      console.log("pressed a key");
+      break;
+    case "s":
+      console.log("pressed s key");
+      break;
+    case "d":
+      console.log("pressed d key");
+      break;
+  }
+});
