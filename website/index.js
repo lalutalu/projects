b6fa6c53e960565be4c9ac70c3971aeb6913930d
Lukas Alutalu <alutalu.lukas@outlook.com>
2022-06-16 16:44:13 -0400

    slowly trucking on

--- website/index.js
+++ website/index.js
@@ -1,15 +1,47 @@
+// Make HTML element canvas appear
 const canvas = document.querySelector('canvas')
 const c = canvas.getContext('2d')
 
+// size of canvas
 canvas.width = 1024
 canvas.height = 576
 
 c.fillStyle = 'white'
 c.fillRect(0, 0, canvas.width, canvas.height)
 
+// background png
 const image = new Image()
 image.src = './img/Pellet Town.png'
-c.drawImage(image, 0, 0)
+
+// 4 side by side playerDown Image
+const playerImage = new Image()
+playerImage.src = './img/playerDown.png'
+
+// Make background and player appear centered on screen 
 image.onload = () => {
-    c.drawImage(image, 0, 0)
+    c.drawImage(image, -750, -600)
+    c.drawImage(
+        
+        // cropping
+        playerImage,
+        0,
+        0,
+        playerImage.width / 4,
+        playerImage.height,
+       
+        // actual width and height of image rendered on screen
+        canvas.width / 2 - playerImage.width / 4,
+        canvas.height / 2 - playerImage.height / 2,
+        playerImage.width / 4,
+        playerImage.height,
+
+    )
 }
+
+window.addEventListener('keydown', (e) => {
+    switch (e.key) {
+        case: 'w': 
+
+            break
+    }
+})
