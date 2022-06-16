0d6ddbf0cdb9d9c66f98c8538a09d4efe28f7b40
Lukas Alutalu <alutalu.lukas@outlook.com>
2022-06-16 15:10:30 -0400

    first image on canvas

--- website/index.js
+++ website/index.js
@@ -1,2 +1,15 @@
+const canvas = document.querySelector('canvas')
+const c = canvas.getContext('2d')
 
-console.log('hello world')
+canvas.width = 1024
+canvas.height = 576
+
+c.fillStyle = 'white'
+c.fillRect(0, 0, canvas.width, canvas.height)
+
+const image = new Image()
+image.src = './img/Pellet Town.png'
+c.drawImage(image, 0, 0)
+image.onload = () => {
+    c.drawImage(image, 0, 0)
+}
